be20a8e69b538a90ce717778955d93b3e540cf18
Carla Holcomb <carla.programmer@gmail.com>
2020-05-29 02:30:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const append = ["Milo", "Otis", "Garfield"];
 append.push('Odie')
 const prepend = ["Milo", "Otis", "Garfield"];
-prepend.
+prepend.unshift('')
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
